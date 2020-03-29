9c1f8c0b7db66e558d4f1bbe68564eabc1cac979
Frank Showalter <fshowalter@gmail.com>
2020-03-29 11:28:42 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ brew bundle
 rm -rf ./.venv
 rm -rf movielog.egg-info
 python3 -m venv ./.venv
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 source ./.venv/bin/activate
-poetry install
+pip install -r requirements.txt
 pip install -e .
