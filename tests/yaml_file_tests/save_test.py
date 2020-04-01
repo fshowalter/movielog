10e7f0a30c9bc9a7ce0aeaadedec3e78ae7e6c74
Frank Showalter <fshowalter@gmail.com>
2020-04-01 10:31:07 -0400

    add tests

--- tests/yaml_file_tests/save_test.py
+++ tests/yaml_file_tests/save_test.py
@@ -38,12 +38,33 @@ def test_logs_save(tmp_path: str, mocker: MockFixture) -> None:
     movielog.logger.logger.log.assert_called_once_with("Wrote {}", file_path)  # type: ignore
 
 
-def test_custom_log_function() -> None:
-    pass
+def test_custom_log_function(tmp_path: str, mocker: MockFixture) -> None:
+    file_path = os.path.join(tmp_path, "test_writes_yaml.yaml")
+
+    custom_log_function = mocker.stub(name="custom_log_function")
+
+    movie = ConcreteMovie(
+        file_path=file_path, imdb_id="tt0053221", title="Rio Bravo", year=1959,
+    )
+    movie.save(custom_log_function)
+
+    custom_log_function.assert_called_once()
 
 
-def test_handles_unicode() -> None:
-    pass
+def test_handles_unicode(tmp_path: str) -> None:
+    expected = "imdb_id: tt0309832\ntitle: Maléfique\n"
+
+    file_path = os.path.join(tmp_path, "test_handles_unicode.yaml")
+
+    movie = ConcreteMovie(
+        file_path=file_path, imdb_id="tt0309832", title="Maléfique", year=1959,
+    )
+    movie.save()
+
+    with open(str(file_path), "r") as output_file:
+        yaml_content = output_file.read()
+
+    assert yaml_content == expected
 
 
 def test_writes_yaml(tmp_path: str) -> None:
@@ -82,3 +103,30 @@ def test_creates_slug_if_no_filename(tmp_path: str, mocker: MockFixture) -> None
         yaml_content = output_file.read()
 
     assert yaml_content == expected
+
+
+def test_creates_directory_if_not_present(tmp_path: str, mocker: MockFixture) -> None:
+    expected = "imdb_id: tt0053221\ntitle: Rio Bravo\n"
+
+    mocker.patch.object(ConcreteMovie, "folder_path", lambda: tmp_path)
+
+    movie = ConcreteMovie(
+        file_path=None, imdb_id="tt0053221", title="Rio Bravo", year=1959,
+    )
+
+    mocker.patch.object(
+        movie, "generate_slug", lambda: "test_creates_slug_if_no_filename"
+    )
+
+    mocker.patch("os.path.exists").return_value = False
+    mocker.patch("os.makedirs")
+
+    movie.save()
+
+    file_path = os.path.join(tmp_path, "test_creates_slug_if_no_filename.yml")
+
+    with open(file_path, "r") as output_file:
+        yaml_content = output_file.read()
+
+    assert yaml_content == expected
+    os.makedirs.assert_called_once_with(str(tmp_path))  # type: ignore
