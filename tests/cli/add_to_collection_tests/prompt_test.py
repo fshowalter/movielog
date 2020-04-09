f680eacbafaaa08ac50602c76a535d61f6b61120
Frank Showalter <fshowalter@gmail.com>
2020-04-09 11:20:05 -0400

    add tests

--- tests/cli/add_to_collection_tests/prompt_test.py
+++ tests/cli/add_to_collection_tests/prompt_test.py
@@ -5,6 +5,7 @@ from pytest_mock import MockFixture
 
 from movielog import watchlist
 from movielog.cli import add_to_collection
+from movielog.watchlist_file import Title
 from tests.cli.keys import Down, Enter, Escape
 from tests.cli.typehints import MovieTuple, PosixPipeInput
 
@@ -23,6 +24,14 @@ def seed_db(seed_movies: Callable[[List[MovieTuple]], None]) -> None:
                 [("nm0000397", "Corey Feldman")],
             ),
             ("tt0089175", "Fright Night", 1985, [("nm0001697", "Chris Sarandon")]),
+            ("tt0080761", "Friday the 13th", 1980, [("nm0658133", "Betsy Palmer")]),
+            ("tt0082418", "Friday the 13th Part 2", 1981, [("nm0824386", "Amy Steel")]),
+            (
+                "tt0083972",
+                "Friday the 13th Part III",
+                1982,
+                [("nm0050676", "Terry Ballard")],
+            ),
             ("tt0116671", "Jack Frost", 1997, [("nm0531924", "Scott MacDonald")]),
         ]
     )
@@ -30,11 +39,19 @@ def seed_db(seed_movies: Callable[[List[MovieTuple]], None]) -> None:
 
 @pytest.fixture(autouse=True)
 def mock_collection_add_title(mocker: MockFixture) -> MockFixture:
-    collection = Collection(name="Friday the 13th")
+    collection = Collection(
+        name="Friday the 13th",
+        titles=[
+            Title(imdb_id="tt0080761", year=1980, title="Friday the 13th"),
+            Title(imdb_id="tt0082418", year=1981, title="Friday the 13th Part 2"),
+            Title(imdb_id="tt0083972", year=1982, title="Friday the 13th Part III"),
+        ],
+    )
     mocker.patch(
         "movielog.cli.add_to_collection.watchlist.all_collections",
         return_value=[collection],
     )
+    mocker.patch.object(collection, "save")
     return mocker.patch.object(collection, "add_title")
 
 
@@ -42,7 +59,7 @@ def test_calls_add_title_on_collection(
     mock_input: PosixPipeInput, mock_collection_add_title: MockFixture
 ) -> None:
     mock_input.send_text(
-        f"{Down}{Enter}Friday the 13th{Enter}{Down}{Enter}y{Enter}"
+        f"{Down}{Enter}The Final Chapter{Enter}{Down}{Enter}y{Enter}"  # noqa: WPS221
     )  # noqa: WPS221
     add_to_collection.prompt()
 
