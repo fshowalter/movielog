70d644417eda801de9805ec1d946ccbb7410a7e3
Frank Showalter <fshowalter@gmail.com>
2020-10-04 02:04:48 -0400

    fix tests

--- tests/viewings_tests/conftest.py
+++ tests/viewings_tests/conftest.py
@@ -7,3 +7,8 @@ from pytest_mock import MockerFixture
 @pytest.fixture(autouse=True)
 def performing_credits_update_mock(mocker: MockerFixture) -> MagicMock:
     return mocker.patch("movielog.viewings.performing_credits.update")
+
+
+@pytest.fixture(autouse=True)
+def release_dates_update_mock(mocker: MockerFixture) -> MagicMock:
+    return mocker.patch("movielog.viewings.release_dates.update")
--- tests/watchlist_tests/conftest.py
+++ tests/watchlist_tests/conftest.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 from pytest_mock import MockerFixture
 
@@ -5,3 +7,8 @@ from pytest_mock import MockerFixture
 @pytest.fixture(autouse=True)
 def mock_imdb_call(mocker: MockerFixture) -> None:
     mocker.patch("movielog.imdb_http.credits_for_person", return_value=[])
+
+
+@pytest.fixture(autouse=True)
+def release_dates_update_mock(mocker: MockerFixture) -> MagicMock:
+    return mocker.patch("movielog.watchlist.release_dates.update")
