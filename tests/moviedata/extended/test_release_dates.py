f1068ceb15116ea6f7aa360d054e28ef413d35f8
Frank Showalter <fshowalter@gmail.com>
2021-06-07 15:59:55 -0400

    add test

--- tests/moviedata/extended/test_release_dates.py
+++ tests/moviedata/extended/test_release_dates.py
@@ -11,3 +11,13 @@ def test_parse_returns_empty_release_date_if_no_raw_release_dates() -> None:
     expected = release_dates.ReleaseDate(date=date(2001, 1, 1), notes="No release date")
 
     assert expected == release_dates.parse(movie)
+
+
+def test_parse_returns_empty_release_date_if_raw_release_dates_malformed() -> None:
+    movie = imdb.Movie.Movie(
+        data={"year": 2001, "raw release dates": [{"bad_json": 2001}]}
+    )
+
+    expected = release_dates.ReleaseDate(date=date(2001, 1, 1), notes="No release date")
+
+    assert expected == release_dates.parse(movie)
