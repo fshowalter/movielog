d6b707bac73d0350e0136c5681afc065f77435e4
Frank Showalter <fshowalter@gmail.com>
2021-06-07 15:53:34 -0400

    add test

--- tests/watchlist/test_api.py
+++ tests/watchlist/test_api.py
@@ -1,6 +1,11 @@
 from pytest_mock import MockerFixture
 
 from movielog.watchlist import api as watchlist_api
+from movielog.watchlist import movies, serializer
+from movielog.watchlist.collections import Collection
+from movielog.watchlist.directors import Director
+from movielog.watchlist.performers import Performer
+from movielog.watchlist.writers import Writer
 
 
 def test_refresh_credits_refreshes_all_types(mocker: MockerFixture) -> None:
@@ -40,3 +45,77 @@ def test_export_data_calls_watchlist_table_update_with_all_people_and_collection
     watchlist_table_mock.assert_called_once_with(
         [director, performer, writer], [collection]
     )
+
+
+def test_movies_returns_all_people_and_collections_movies() -> None:
+
+    serializer.serialize(
+        Director(
+            frozen=True,
+            imdb_id="nm0001328",
+            name="Howard Hawks",
+            slug="howard-hawks",
+            movies=[
+                movies.Movie(
+                    title="Rio Bravo", year=1959, imdb_id="nm0001328", notes=""
+                )
+            ],
+        )
+    )
+
+    serializer.serialize(
+        Writer(
+            frozen=True,
+            imdb_id="nm0102824",
+            name="Leigh Brackett",
+            slug="leigh-brackett",
+            movies=[
+                movies.Movie(
+                    imdb_id="tt0038355", title="The Big Sleep", year=1946, notes=""
+                )
+            ],
+        )
+    )
+
+    serializer.serialize(
+        Performer(
+            frozen=True,
+            imdb_id="nm0000078",
+            name="John Wayne",
+            slug="john-wayne",
+            movies=[
+                movies.Movie(
+                    title="Rio Bravo", year=1959, imdb_id="nm0001328", notes=""
+                )
+            ],
+        )
+    )
+
+    serializer.serialize(
+        Collection(
+            name="Vampire Movies",
+            slug="vampire-movies",
+            movies=[
+                movies.Movie(
+                    imdb_id="tt0089175",
+                    title="Fright Night",
+                    year=1985,
+                    notes="",
+                )
+            ],
+        )
+    )
+
+    expected = [
+        movies.Movie(title="Rio Bravo", year=1959, imdb_id="nm0001328", notes=""),
+        movies.Movie(title="Rio Bravo", year=1959, imdb_id="nm0001328", notes=""),
+        movies.Movie(imdb_id="tt0038355", title="The Big Sleep", year=1946, notes=""),
+        movies.Movie(
+            imdb_id="tt0089175",
+            title="Fright Night",
+            year=1985,
+            notes="",
+        ),
+    ]
+
+    assert expected == watchlist_api.movies()
