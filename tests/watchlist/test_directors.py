528c1495aca7bbb124dc99665abc41fdacf40cee
Frank Showalter <fshowalter@gmail.com>
2021-06-08 11:08:08 -0400

    add tests

--- tests/watchlist/test_directors.py
+++ tests/watchlist/test_directors.py
@@ -6,6 +6,7 @@ import pytest
 from pytest_mock import MockerFixture
 
 from movielog.watchlist import api as watchlist_api
+from movielog.watchlist import directors, serializer
 from movielog.watchlist.movies import Movie
 
 
@@ -20,7 +21,7 @@ def mock_movies_for_director(mocker: MockerFixture) -> None:
         )
     ]
     mocker.patch(
-        "movielog.watchlist.directors.movies_for_director",
+        "movielog.watchlist.directors.filmography.for_director",
         MagicMock(return_value=movies),
     )
 
@@ -44,6 +45,71 @@ def test_add_director_serializes_director(
     }
 
     watchlist_api.add_director(imdb_id="nm0000080", name="Orson Welles")
+
+    with open(
+        os.path.join(tmp_path, "directors", "orson-welles.json"), "r"
+    ) as output_file:
+        file_content = json.load(output_file)
+
+    assert file_content == expected
+
+
+def test_refresh_movies_adds_new_movies(tmp_path: str) -> None:
+    expected = {
+        "frozen": False,
+        "name": "Orson Welles",
+        "imdb_id": "nm0000080",
+        "slug": "orson-welles",
+        "movies": [
+            {
+                "title": "Citizen Kane",
+                "year": 1941,
+                "imdb_id": "tt0033467",
+                "notes": None,
+            }
+        ],
+    }
+
+    serializer.serialize(
+        directors.Director(
+            frozen=False,
+            name="Orson Welles",
+            imdb_id="nm0000080",
+            slug="orson-welles",
+            movies=[],
+        )
+    )
+
+    directors.refresh_movies()
+
+    with open(
+        os.path.join(tmp_path, "directors", "orson-welles.json"), "r"
+    ) as output_file:
+        file_content = json.load(output_file)
+
+    assert file_content == expected
+
+
+def test_refresh_does_not_update_frozen_entries(tmp_path: str) -> None:
+    expected = {
+        "frozen": True,
+        "name": "Orson Welles",
+        "imdb_id": "nm0000080",
+        "slug": "orson-welles",
+        "movies": [],
+    }
+
+    serializer.serialize(
+        directors.Director(
+            frozen=True,
+            name="Orson Welles",
+            imdb_id="nm0000080",
+            slug="orson-welles",
+            movies=[],
+        )
+    )
+
+    directors.refresh_movies()
 
     with open(
         os.path.join(tmp_path, "directors", "orson-welles.json"), "r"
