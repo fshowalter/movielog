be5038e57af58ed624c24ad409fc46fae3b08496
Frank Showalter <fshowalter@gmail.com>
2021-06-08 11:14:02 -0400

    add tests

--- tests/watchlist/test_writers.py
+++ tests/watchlist/test_writers.py
@@ -6,6 +6,7 @@ import pytest
 from pytest_mock import MockerFixture
 
 from movielog.watchlist import api as watchlist_api
+from movielog.watchlist import serializer, writers
 from movielog.watchlist.movies import Movie
 
 
@@ -44,6 +45,75 @@ def test_add_writer_serializes_writer(
     }
 
     watchlist_api.add_writer(imdb_id="nm0102824", name="Leigh Brackett")
+
+    with open(
+        os.path.join(tmp_path, "writers", "leigh-brackett.json"), "r"
+    ) as output_file:
+        file_content = json.load(output_file)
+
+    assert file_content == expected
+
+
+def test_refresh_movies_adds_new_movies(
+    tmp_path: str,
+) -> None:
+    expected = {
+        "frozen": False,
+        "name": "Leigh Brackett",
+        "imdb_id": "nm0102824",
+        "slug": "leigh-brackett",
+        "movies": [
+            {
+                "title": "The Big Sleep",
+                "year": 1946,
+                "imdb_id": "tt0038355",
+                "notes": None,
+            }
+        ],
+    }
+
+    serializer.serialize(
+        writers.Writer(
+            frozen=False,
+            name="Leigh Brackett",
+            slug="leigh-brackett",
+            imdb_id="nm0102824",
+            movies=[],
+        )
+    )
+
+    writers.refresh_movies()
+
+    with open(
+        os.path.join(tmp_path, "writers", "leigh-brackett.json"), "r"
+    ) as output_file:
+        file_content = json.load(output_file)
+
+    assert file_content == expected
+
+
+def test_refresh_movies_does_not_update_frozen_entries(
+    tmp_path: str,
+) -> None:
+    expected = {
+        "frozen": True,
+        "name": "Leigh Brackett",
+        "imdb_id": "nm0102824",
+        "slug": "leigh-brackett",
+        "movies": [],
+    }
+
+    serializer.serialize(
+        writers.Writer(
+            frozen=True,
+            name="Leigh Brackett",
+            slug="leigh-brackett",
+            imdb_id="nm0102824",
+            movies=[],
+        )
+    )
+
+    writers.refresh_movies()
 
     with open(
         os.path.join(tmp_path, "writers", "leigh-brackett.json"), "r"
