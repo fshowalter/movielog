7bb29ca2d0de22b535ac9f66e5262e0dfd29a269
Frank Showalter <fshowalter@gmail.com>
2021-06-10 09:58:06 -0400

    add test

--- tests/utils/test_format_tools.py
+++ tests/utils/test_format_tools.py
@@ -7,6 +7,25 @@ from movielog.utils import format_tools
 def test_humanize_int_adds_comma_when_appropriate(
     test_input: int, expected: str
 ) -> None:
-    test_result = format_tools.humanize_int(test_input)
+    assert format_tools.humanize_int(test_input) == expected
 
-    assert test_result == expected
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (1.678, "1.7B"),
+        (106, "106.0B"),
+        (3000.5, "2.9KiB"),
+        (86780.0, "84.7KiB"),
+        (445478.34, "435.0KiB"),
+        (5866458.34, "5.6MiB"),
+        (36788726.0, "35.1MiB"),
+        (835488726.0, "796.8MiB"),
+        (1987654852.0, "1.9GiB"),
+        (325877892548668857899255685872.0, "269559.9YiB"),
+    ],
+)
+def test_pretty_file_size_returns_humanized_sizes(
+    test_input: float, expected: str
+) -> None:
+    assert format_tools.pretty_file_size(test_input) == expected
