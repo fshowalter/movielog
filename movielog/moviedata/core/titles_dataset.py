340a9152a7cf38c5ece24920ceaa28cfe3d092f4
Frank Showalter <fshowalter@gmail.com>
2021-10-25 16:11:21 -0400

    add/update reviews

--- movielog/moviedata/core/titles_dataset.py
+++ movielog/moviedata/core/titles_dataset.py
@@ -18,6 +18,7 @@ Whitelist = {
     "tt0093135",  # Hack-O-Lantern (1988) [V]
     "tt11060882",  # Batman: The Dark Knight Returns (2013) [V]
     "tt0101760",  # Door to Silence (1992) [V]
+    "tt0112643",  # Castle Freak (1995) [V]
 }
 
 MovieRow = movies_table.Row
