503fbfaa4e06e4254b2196a2e8df4e3cb9e0f1e2
Frank Showalter <fshowalter@gmail.com>
2023-12-08 09:02:57 -0500

    fix tests

--- tests/cli/test_add_writer.py
+++ tests/cli/test_add_writer.py
@@ -4,88 +4,61 @@ import pytest
 from pytest_mock import MockerFixture
 
 from movielog.cli import add_writer
-from movielog.moviedata.core import movies_table, people_table
+from movielog.repository.datasets.dataset_name import DatasetName
+from movielog.repository.db import names_table
 from tests.cli.conftest import MockInput
 from tests.cli.keys import Down, Enter, Escape
 
 
 @pytest.fixture(autouse=True)
-def mock_add_writer(mocker: MockerFixture) -> MagicMock:
-    return mocker.patch("movielog.cli.add_writer.movielog_api.add_writer")
+def mock_add_person_to_watchlist(mocker: MockerFixture) -> MockerFixture:
+    return mocker.patch(
+        "movielog.cli.add_director.repository_api.add_person_to_watchlist"
+    )
 
 
 @pytest.fixture(autouse=True)
 def seed_db() -> None:
-    people_table.reload(
+    names_table.reload(
         [
-            people_table.Row(
+            DatasetName(
                 imdb_id="nm0762727",
                 full_name="Jimmy Sangster",
-                known_for_title_ids="tt0051554,tt0050280",
+                known_for_titles=["Horror of Dracula", "The Curse of Frankenstein"],
             ),
-            people_table.Row(
+            DatasetName(
                 imdb_id="nm0102824",
                 full_name="Leigh Brackett",
-                known_for_title_ids="tt0053221,tt0000000",
+                known_for_titles=[
+                    "The Big Sleep",
+                    "Rio Bravo",
+                    "The Empire Strikes Back",
+                ],
             ),
-            people_table.Row(
+            DatasetName(
                 imdb_id="nm0276169",
                 full_name="Tom Holland",
-                known_for_title_ids="tt0089175",
-            ),
-        ]
-    )
-
-    movies_table.reload(
-        [
-            movies_table.Row(
-                imdb_id="tt0051554",
-                title="Horror of Dracula",
-                original_title="Dracula",
-                year=1958,
-                runtime_minutes=None,
-                principal_cast_ids=None,
-                votes=32,
-                imdb_rating=4.5,
-            ),
-            movies_table.Row(
-                imdb_id="tt0053221",
-                title="Rio Bravo",
-                original_title="Rio Bravo",
-                year=1959,
-                runtime_minutes=None,
-                principal_cast_ids=None,
-                votes=23,
-                imdb_rating=8.1,
-            ),
-            movies_table.Row(
-                imdb_id="tt0089175",
-                title="Fright Night",
-                original_title="Fright Night",
-                year=1985,
-                runtime_minutes=None,
-                principal_cast_ids=None,
-                votes=16,
-                imdb_rating=4,
+                known_for_titles=["Fright Night", "Childs Play"],
             ),
         ]
     )
 
 
-def test_calls_add_writer(mock_input: MockInput, mock_add_writer: MagicMock) -> None:
+def test_calls_add_writer(
+    mock_input: MockInput, mock_add_person_to_watchlist: MagicMock
+) -> None:
     mock_input(["Leigh Brackett", Enter, Down, Enter, "y", Enter])
     add_writer.prompt()
 
-    mock_add_writer.assert_called_once_with(
-        imdb_id="nm0102824",
-        name="Leigh Brackett",
+    mock_add_person_to_watchlist.assert_called_once_with(
+        imdb_id="nm0102824", name="Leigh Brackett", watchlist="writers"
     )
 
 
 def test_does_not_call_add_writer_if_no_selection(
-    mock_input: MockInput, mock_add_writer: MagicMock
+    mock_input: MockInput, mock_add_person_to_watchlist: MagicMock
 ) -> None:
     mock_input([Escape, Enter])
     add_writer.prompt()
 
-    mock_add_writer.assert_not_called()
+    mock_add_person_to_watchlist.assert_not_called()
