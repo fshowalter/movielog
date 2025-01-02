554c4b327c8971f61c3ed045dfbc2ce0509d5b51
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-01-01 22:14:08 -0500

    revert

--- scripts/add_legacy_reviews.py
+++ scripts/add_legacy_reviews.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
 import datetime
+import os
 import re
 import sqlite3
 from contextlib import contextmanager
-from os import path
 from typing import Any, Callable, Dict, Generator, Tuple
 
+import yaml
+
 from movielog.repository import api as repository_api
-from movielog.utils import list_tools
+from movielog.utils import list_tools, path_tools
 from movielog.utils.logging import logger
 
+FOLDER_NAME = "reviews"
 DB_FILE_NAME = "sabren.sqlite3"
 DB_DIR = "db"
 
@@ -21,6 +24,10 @@ Row = sqlite3.Row
 DB_PATH = path.join(DB_DIR, DB_FILE_NAME)
 DbConnectionOpts: Dict[str, Any] = {"isolation_level": None}
 RowFactory = Callable[[sqlite3.Cursor, Tuple[Any, ...]], Any]
+
+
+def _represent_none(self: Any, _: Any) -> Any:
+    return self.represent_scalar("tag:yaml.org,2002:null", "")
 
 
 @contextmanager
@@ -40,6 +47,46 @@ def fetch_all(query: str, row_factory: RowFactory = sqlite3.Row) -> list[Any]:
     with connect() as connection:
         connection.row_factory = row_factory
         return connection.execute(query).fetchall()
+
+
+def _generate_file_path(slug: str) -> str:
+    file_path = os.path.join(FOLDER_NAME, "{0}.md".format(slug))
+
+    path_tools.ensure_file_path(file_path)
+
+    return file_path
+
+
+def create_review(
+    slug: str, date: datetime.date, review_content: str, grade: str
+) -> str:
+    yaml.add_representer(type(None), _represent_none)
+
+    file_path = _generate_file_path(slug)
+
+    stripped_content = str(review_content or "").strip()
+
+    with open(file_path, "w") as output_file:
+        output_file.write("---\n")
+        yaml.dump(
+            {
+                "imdb_id": None,
+                "slug": slug,
+                "grade": grade,
+                "date": date,
+            },
+            encoding="utf-8",
+            allow_unicode=True,
+            default_flow_style=False,
+            sort_keys=False,
+            stream=output_file,
+        )
+        output_file.write("---\n\n")
+        output_file.write(stripped_content)
+
+    logger.log("Wrote {}", file_path)
+
+    return file_path
 
 
 def add_legacy_reviews() -> None:  # noqa: WPS210, WPS231
@@ -66,28 +113,30 @@ def add_legacy_reviews() -> None:  # noqa: WPS210, WPS231
         if slug in reviews.keys():
             continue
 
-        logger.log(slug)
-        continue
-
         for post_meta_row in get_post_meta_for_id(review_row["ID"]):
             for key in post_meta_row.keys():
                 "{0}:{1}".format(key, post_meta_row[key])
             if post_meta_row["meta_key"] == "Date Viewed":
                 viewing_dates.append(post_meta_row["meta_value"])
+            if post_meta_row["meta_key"] == "Grade":
+                grade = post_meta_row["meta_value"]
 
-        title = reviews[slug].title()
+        create_review(
+            slug=slug,
+            date=viewing_dates[-1],
+            review_content=review_row["post_content"],
+            grade=grade,
+        )
 
-        logger.log(viewing_dates[-1])
-
-        for viewing_date in viewing_dates:
-            repository_api.create_viewing(
-                imdb_id=title.imdb_id,
-                full_title="{0} ({1})".format(title.title, title.year),
-                date=datetime.datetime.fromisoformat(viewing_date).date(),
-                medium=None,
-                venue=None,
-                medium_notes=None,
-            )
+        # for viewing_date in viewing_dates:
+        #     repository_api.create_viewing(
+        #         imdb_id=title.imdb_id,
+        #         full_title="{0} ({1})".format(title.title, title.year),
+        #         date=datetime.datetime.fromisoformat(viewing_date).date(),
+        #         medium=None,
+        #         venue=None,
+        #         medium_notes=None,
+        #     )
 
 
 def get_post_meta_for_id(id: str) -> Any:
@@ -106,6 +155,7 @@ def get_review_rows() -> list[Any]:
     query = """
         SELECT
         post_title
+        , post_content
         , post_name
         , id
         FROM wp_posts
