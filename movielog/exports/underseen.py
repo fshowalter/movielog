c395a55a86b36adf000ac36ac5762f091a370c4b
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-11-29 10:44:13 -0500

    Adjust grade thresholds for overrated, underrated, and underseen exports

--- movielog/exports/underseen.py
+++ movielog/exports/underseen.py
@@ -20,7 +20,7 @@ def export(repository_data: RepositoryData) -> None:
         title = repository_data.titles[imdb_id]
         review = repository_data.reviews[title.imdb_id]
 
-        if not review.grade_value or review.grade_value < 8:
+        if not review.grade_value or review.grade_value < 12:
             continue
 
         underseen_gems.append(
