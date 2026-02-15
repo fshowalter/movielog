acdaa20dabd82baac475efd397702d9449a9ede5
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-02-15 08:18:20 -0500

    refactor: replace TypeVar with PEP 695 generic syntax

--- movielog/utils/list_tools.py
+++ movielog/utils/list_tools.py
@@ -2,13 +2,9 @@ from __future__ import annotations
 
 from collections import defaultdict
 from collections.abc import Callable, Iterable
-from typing import TypeVar
-
-ListType = TypeVar("ListType")
-KeyType = TypeVar("KeyType")
 
 
-def group_list_by_key(
+def group_list_by_key[ListType, KeyType](
     iterable: Iterable[ListType], key: Callable[[ListType], KeyType]
 ) -> dict[KeyType, list[ListType]]:
     items_by_key = defaultdict(list)
@@ -19,7 +15,7 @@ def group_list_by_key(
     return items_by_key
 
 
-def list_to_dict(
+def list_to_dict[ListType, KeyType](
     iterable: Iterable[ListType], key: Callable[[ListType], KeyType]
 ) -> dict[KeyType, ListType]:
     items_by_key: dict[KeyType, ListType] = {}
